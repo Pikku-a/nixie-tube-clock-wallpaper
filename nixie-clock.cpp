d6906e5dc2ceed6a402272e8ef2f9af698fef484
pikku-a <aapo1mattila@gmail.com>
2023-01-21 22:25:32 +0200

    initial commit

---
+++ nixie-clock.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <ctime>		//For getting time
+#include <windows.h>	//For changing wallpaper
+//#include <stdlib.h>	//For linux cmd to use a cmd command to change wallpaper
+//#include <chrono>		//Not necessary probably
+//#include <Magick++.h>	//For editing the image
+
+//using namespace Magick;
+using namespace std;
+
+int main() {
+	
+	//GET TIME - Move this to a different script?
+	time_t currentTime;
+	struct tm *localTime;
+
+	time(&currentTime);                   // Get the current time
+	localTime = localtime(&currentTime);  // Convert the current time to the local time
+
+	int hour = localTime->tm_hour;
+	int min = localTime->tm_min;
+	int sec = localTime->tm_sec;
+
+	cout << "Current time is : " << hour << ":" << min << ":" << sec << "\n";
+	
+	/*time_t t = time(0); //Get time
+	time_t t2 = chrono::system_clock::to_time_t(chrono::system_clock::now());
+	t3 = now.time_of_day().hours;
+	cout << "Time 1: " << ctime(&t) << "\n";
+	cout << "Time 2: " << ctime(&t2) << "\n";
+	cout << "Time 3: " << t3 << "\n";*/
+	
+	
+	//CREATE WALLPAPER IMAGE
+	Image image;
+	try {
+		//Read a file into image object
+		image.read("pics/bg.png");
+		
+		//Do things
+		
+		//Vars
+		int loc = 410; //Location
+		int y = 360; //?
+		int imgNum = 0; //image number
+		
+		if (get_digit_count(hour)) == 1) {
+            //pics.append("blank.png");
+		}
+		
+        for (imgNum=0;i<2;i++) {
+            pics.append(v);
+		}
+        //pics.append("period.png")
+		
+		if (get_digit_count(min)) == 1) {
+            //pics.append("blank.png");
+		}
+        for v in str(start.minute):
+            //pics.append(v)
+        //pics.append("period.png")
+		
+		if (get_digit_count(sec)) == 1) {
+            //pics.append("blank.png");
+		}
+        for v in str(start.second):
+            pics.append(v)
+		
+		//Write the image to a file
+		image.write("final.png");
+	}
+	catch(Exception %error_) {
+		cout << "Caught exception: " << error_.what() << "\n";
+		return 1;
+	}
+	
+	
+	//SET WALLPAPER
+	
+	//In windows
+    const wchar_t *path = L"pics\final.png";
+    int result;
+    result = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void *)path, SPIF_UPDATEINIFILE);
+    if (result==true) {
+		cout << "Changed wallpaper successfully" << "\n";
+	}else{
+		cout << "Could not change wallpaper" << "\n";
+	}
+	
+	//In gnome (linux) (change the file location)
+	//system(gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/ubuntu-default-greyscale-wallpaper.png)
+	
+	
+	return 0;
+	
+}
+
+//get_digit_amount script
+int get_digit_count(int number) {
+   int count = 0;
+   while(number != 0) {
+	  number = number / 10;
+	  count++;
+   }
+   return count;
+}
