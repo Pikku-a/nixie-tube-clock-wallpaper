c82f0f44e25467839a12db4b34f22618ac5eecd4
Aap0 <a4po@proton.me>
2024-04-10 14:56:39 +0300

    Fixes, cinnamon version and cleaning code

--- nixie-clock.cpp
+++ nixie-clock.cpp
@@ -1,17 +1,17 @@
 
 // Nixie tube clock live wallpaper
-// © Pikku-a 2023
+// © Pikku-a 2023-2024
 
 #include <iostream>
-#include <ctime>			//For getting time
-//#include <windows.h>		//For changing wallpaper
-#include <stdlib.h>			//For linux cmd to use a cmd command to change wallpaper
-#include <Magick++.h>		//For editing the image		//"C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\include\Magick++.h" ?
+#include <ctime>			// For getting time
+//#include <windows.h>		// For changing wallpaper
+#include <stdlib.h>			// For linux cmd to use a cmd command to change wallpaper
+#include <Magick++.h>		// For editing the image		//"C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\include\Magick++.h" ?
 #include <string>
-#include <chrono>			//This and the one below are for sleep_for milliseconds
+#include <chrono>			// This and the one below are for sleep_for milliseconds
 #include <thread>
-#include <unistd.h>			//This
-#include <linux/limits.h>	//and this are for getting the path to the program directory
+#include <unistd.h>			// This
+#include <linux/limits.h>	// and this are for getting the path to the program directory
 
 using namespace Magick;
 using namespace std;
@@ -31,12 +31,15 @@ string get_selfpath() {
 		
 int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only necessary for windows
 	
-	//Initialize imagemagick (only necessary for windows)
+	// Check that libraries needed for imagemagick++ are installed and if not, install them
+	//
+
+	// Initialize imagemagick (only necessary for windows)
 	//InitializeMagick(*argv); //C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\ 
 	
 	Magick::InitializeMagick(nullptr);
 	
-	//Get path to program directory and add "/pics/final.png" to it
+	// Get path to program directory and add "/pics/final.png" to it
 	string selfpath = get_selfpath();
 	string::size_type t = selfpath.find_last_of("/");
 	selfpath = selfpath.substr(0,t);
@@ -46,9 +49,9 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 	
 	while(true) {
 		
-		//Check here if wallpaper is visible or if there is no active window and only execute code if it's true? (couldn't find a way to do this in linux (at least not in wayland))
+		// Check here if wallpaper is visible or if there is no active window and only execute code if it's true? (couldn't find a way to do this in linux (at least not in wayland))
 		
-		//GET TIME - Move this to a different script?
+		// GET TIME - Move this to a different script?
 		time_t currentTime;
 		struct tm *localTime;
 
@@ -62,29 +65,29 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 		cout << "Current time is : " << hour << ":" << min << ":" << sec << "\n";
 		
 		
-		//CREATE WALLPAPER IMAGE
+		// CREATE WALLPAPER IMAGE
 		
-		//Another option would be to use batch or bash - with gimp for example (the AI could use it for converting files to other formats for example (e.g.: https://www.gimp.org/tutorials/AutomatedJpgToXcf/))
+		// Another option would be to use batch or bash - with gimp for example (the AI assistant project could use it for converting files to other formats for example (e.g.: https://www.gimp.org/tutorials/AutomatedJpgToXcf/))
 		//system("mybatchfile.bat"); or system("mybashfile.sh"); for linux
-		//If someone else than me is reading this, just ignore these comments.
+		// If someone else than me is reading this, just ignore these comments.
 		
-		//Construct the image object
+		// Construct the image object
 		Image image;
 		Image clocknum;
 		
 		try {
 			
-			//Read a file into image object
-			image.read(selfpathFolder+"/pics/bg.png");		//Background image (originally bg.png was used, but I changed it so the period.png:s don't need to be added every time. EDIT: Changed it back for now.)
-			clocknum.read(selfpathFolder+"/pics/blank.png");	//Nixie tube (clock number)
+			// Read a file into image object
+			image.read(selfpathFolder+"/pics/bg.png");		// Background image (originally bg.png was used, but I changed it so the period.png:s don't need to be added every time. EDIT: Changed it back for now.)
+			clocknum.read(selfpathFolder+"/pics/blank.png");	// Nixie tube (clock number)
 			
-			//Variables
+			// Variables
 			int x = 290; 		//image x position (410 originally)
 			int y = 420; 		//image y position (360 originally)
 			string imgnums[8] = {"blank","blank","period","blank","blank","period","blank","blank"};
 			
-			//Convert numbers to string and add them to the array
-			//Hours
+			// Convert numbers to string and add them to the array
+			// Hours
 			string s = to_string(hour);
 			for (int i=0; i<s.size(); i++) {
 				if (get_digit_count(hour) == 1) {
@@ -95,7 +98,7 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 				}
 				//cout << s[i] << "\n";
 			}
-			//Minutes
+			// Minutes
 			s = to_string(min);
 			for (int i=0;i<s.size();i++) {
 				if (get_digit_count(min) == 1) {
@@ -106,19 +109,19 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 				}
 				//cout << s[i] << "\n";
 			}
-			//Seconds
+			// Seconds
 			s = to_string(sec);
 			for (int i=0;i<s.size();i++) {
 				if (get_digit_count(sec) == 1) {
 					imgnums[i+6] = "blank";
-					imgnums[i+7] = s[i]; //In seconds when it's 0 it draws it to the wrong position for some reason (does the same happen to minutes and hours?)
+					imgnums[i+7] = s[i]; // In seconds when it's 0 it draws it to the wrong position for some reason (does the same happen to minutes and hours?)
 				}else{
 					imgnums[i+6] = s[i];
 				}
 				//cout << s[i] << "\n";
 			}
 			
-			//Paste the number images to the background image
+			// Paste the number images to the background image
 			for (int i=0;i<8;i++) {
 				x+=140;
 				//if (i != 2 && i != 5) { //Don't draw the period.png images to the background so less stuff needs to be drawn every second (do this only if using final.png as base image)
@@ -128,7 +131,7 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 				//}
 			}
 			
-			//Write the image to a file
+			// Write the image to a file
 			image.write(selfpathFolder+"/pics/final.png"); // "/usr/share/backgrounds/final.png"
 			
 		}
@@ -138,7 +141,7 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 		}
 		
 		
-		//SET WALLPAPER
+		// SET WALLPAPER
 		
 		/*/In windows
 		const wchar_t *path = L"pics\final.png";
@@ -151,22 +154,28 @@ int main(/*int argc,char **argv*/) { //arguments are for imagemagick, only neces
 			cout << "Could not change wallpaper" << "\n";
 		}*/
 		
-		//In gnome (linux)
-		system(("gsettings set org.gnome.desktop.background picture-uri "+selfpath).c_str()); // 'pics/final.png' //'file:////usr/share/backgrounds/final.png'
-		//Add alternatives to linux distributions that don't use gnome?
-		
-		cout << "Wallpaper set" << "\n";
-		
-		//Execute everything only every 800 milliseconds for better performance
+		// Gnome - Make this more similar to Cinnamon version
+		//system(("gsettings set org.gnome.desktop.background picture-uri "+selfpath).c_str()); // 'pics/final.png' //'file:////usr/share/backgrounds/final.png'
+		// Cinnamon - In this version there is flickering. Not sure if there is anything I can do to fix that.
+		int ret = system(("gsettings set org.cinnamon.desktop.background picture-uri file://"+selfpath).c_str());
+		if (ret == -1) {
+			cout << "Failed to set wallpaper" << "\n";
+			//cout << "The command used: " << "gsettings set org.cinnamon.desktop.background picture-uri file://" << selfpath.c_str();
+		}else{
+			cout << "Wallpaper set" << "\n";
+			//cout << ret << "\n";
+		}
+
+		// Execute everything only every 800 milliseconds for better performance
 		this_thread::sleep_for(chrono::milliseconds(800)); //Maybe make the number a little lower so it doesn't skip seconds (sometimes rarely)
-		//Also use a variable so it's easier to change?
+		// Also use a variable so it's easier to change?
 		
 	}
 	return 0;
 }
 
 
-//Get amount of digits in number function
+// Get amount of digits in number function
 int get_digit_count(int number) {
    int count = 0;
    while(number != 0) {
@@ -176,9 +185,11 @@ int get_digit_count(int number) {
    return count;
 }
 
-//It seems that sometimes the background has to be changed manuall first before this works
-//Like on cmd: gsettings set org.gnome.desktop.background picture-uri 'path/to/final.png'
+// It seems that sometimes the background has to be changed manuall first before this works
+// Like on cmd: gsettings set org.gnome.desktop.background picture-uri 'path/to/final.png'
 
-//The program can be stopped from terminal with "pkill nixie-clock" - Maybe add some easier way to do it (for example a keyboard shortcut)
+// The program can be stopped from terminal with "pkill nixie-clock" - Maybe add some easier way to do it (for example a keyboard shortcut)
 
-//g++ -o nixie-clock nixie-clock.cpp `Magick++-config --cxxflags --cppflags --ldflags --libs`
+/*
+g++ -o nixie-clock nixie-clock.cpp `Magick++-config --cxxflags --cppflags --ldflags --libs`
+*/
